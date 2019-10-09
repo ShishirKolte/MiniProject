4c74df6394991aaa71cf911f3ce25bf810124633
ShishirKolte <shishirkolte@gmail.com>
2019-10-09 13:36:30 +0530

    Linked Opening Page and Registration Page.

--- app/src/main/java/com/example/collegeevent/MainActivity.java
+++ app/src/main/java/com/example/collegeevent/MainActivity.java
@@ -2,18 +2,55 @@ package com.example.collegeevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login = (Button) findViewById(R.id.bLogin);
+    private TextView register = (TextView) findViewById(R.id.tRegisterRedirect);
 
-    public void nextActivity()
-    {
-        System.exit(0);
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityRegister();
+            }
+        });
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEventView();
+            }
+        });
+
+
+
+
     }
+
+
+    public void openEventView()
+    {
+
+    }
+
+    public void openActivityRegister()
+    {
+        Intent intent;
+        intent = new Intent(this, registerActivity.class);
+        startActivity(intent);
+    }
+
 }

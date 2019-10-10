ac4c7588fb5420398b6a55706dff076c26a3a892
AeshaniPrabhu <aeshani.prabhu@gmail.com>
2019-10-11 00:42:30 +0530

    Firebase authentication dependency added.

--- app/src/main/java/com/example/collegeevent/MainActivity.java
+++ app/src/main/java/com/example/collegeevent/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button login;

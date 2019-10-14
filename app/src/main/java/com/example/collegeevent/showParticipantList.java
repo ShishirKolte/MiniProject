35884cbe22d7310c9e2065b209864531f1bbbb8c
ShreyaGodse <godseshreya@gmail.com>
2019-10-14 15:24:21 +0530

    Logo updated Theme changed Tabs Aligned "show participant list" and "view result" tabs created "download list" and "download result" buttons added!

---
+++ app/src/main/java/com/example/collegeevent/showParticipantList.java
@@ -0,0 +1,14 @@
+package com.example.collegeevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showParticipantList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_participant_list);
+    }
+}

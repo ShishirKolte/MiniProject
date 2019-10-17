24e92d667adf0d42fd9a646394b3d2a8f2dab72a
ShreyaGodse <godseshreya@gmail.com>
2019-10-17 13:22:39 +0530

    "enter result" tab created.

---
+++ app/src/main/java/com/example/collegeevent/resultView.java
@@ -0,0 +1,14 @@
+package com.example.collegeevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_view);
+    }
+}
